980ffcfcb416d68bc7e2ef3812aa51cb8d9a59b4
Patrick Rios <patrick.33.rios@hotmail.com>
2021-03-15 17:45:41 -0400

    Homepage done

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,54 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Pomodoro | DevChallange</title>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"/> 
       </Head>
-      <main>
+      <main className={styles.container}>
         <h1>Pomodoro</h1>
+
+        <section className={styles.countersSection}>
+          <div className={styles.counter}>
+            <div>
+              <img src="./icons/up.svg" alt="Incrementar"/>
+              <img src="./icons/down.svg" alt="Incrementar"/>
+            </div>
+            <div>
+              <strong>25</strong>
+              <h2>Trabalho</h2>
+            </div>
+          </div>
+
+          <div className={styles.counter}>
+            <div>
+              <img src="./icons/up.svg" alt="Incrementar"/>
+              <img src="./icons/down.svg" alt="Incrementar"/>
+            </div>
+            <div>
+              <strong>5</strong>
+              <h2>Pausa</h2>
+            </div>
+          </div>
+
+          <div className={styles.counter}>
+            <div>
+              <img src="./icons/up.svg" alt="Incrementar"/>
+              <img src="./icons/down.svg" alt="Incrementar"/>
+            </div>
+            <div>
+              <strong>3</strong>
+              <h2>Sessões</h2>
+            </div>
+          </div>
+
+        </section>
+
+        <button type="button" className={styles.buttonContinue}>
+          Continuar
+        </button>
       </main>
     </div>
   )
